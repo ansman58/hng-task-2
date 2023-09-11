73bb269b692bfd46ba6e8a5f433c77a744796bed
ansman58 <javanslem@gmail.com>
2023-09-11 19:48:04 +0100

    add more validation

--- app/controllers/UserController.ts
+++ app/controllers/UserController.ts
@@ -2,16 +2,10 @@ import { Request, Response } from "express";
 import { HTTP_STATUS_CODES } from "../constants";
 import { AppDataSource } from "../config/database";
 import { User } from "../models/User";
+import { isAString } from "../utils/general";
 
-const {
-  OK,
-  CREATED,
-  ACCEPTED,
-  NO_CONTENT,
-  BAD_REQUEST,
-  NOT_FOUND,
-  INTERNAL_SERVER_ERROR,
-} = HTTP_STATUS_CODES;
+const { OK, CREATED, ACCEPTED, BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } =
+  HTTP_STATUS_CODES;
 
 export class UserController {
   static async store(req: Request, res: Response) {
@@ -21,20 +15,28 @@ export class UserController {
       return res.status(BAD_REQUEST).json({ message: "Name is required" });
     }
 
+    if (!isAString(name)) {
+      return res.status(BAD_REQUEST).json({ message: "Name must be a string" });
+    }
+
     try {
+      const userModel = AppDataSource.getRepository(User);
+
+      const findUser = await userModel.findOneBy({ name });
+
+      if (findUser) {
+        return res.status(BAD_REQUEST).json({ message: "Name already exists" });
+      }
+
       const user = new User();
       user.name = name;
-      const userModel = AppDataSource.getRepository(User);
       const newUser = await userModel.save(user);
 
       res
         .status(CREATED)
         .json({ message: "User created successfully", newUser });
     } catch (error) {
-      console.log(error);
-      res
-        .status(INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
+      UserController.handleServerError(res, error);
     }
   }
 
@@ -45,10 +47,7 @@ export class UserController {
       const users = await getUsers.find();
       res.status(OK).json({ message: "Users fetched successfully", users });
     } catch (error) {
-      console.log(error);
-      res
-        .status(INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
+      UserController.handleServerError(res, error);
     }
   }
 
@@ -62,10 +61,7 @@ export class UserController {
       }
       res.status(OK).json({ message: "User fetched successfully", user });
     } catch (error) {
-      console.log(error);
-      res
-        .status(INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
+      UserController.handleServerError(res, error);
     }
   }
 
@@ -83,10 +79,7 @@ export class UserController {
 
       res.status(ACCEPTED).json({ message: "User updated successfully", user });
     } catch (error) {
-      console.log(error);
-      res
-        .status(INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
+      UserController.handleServerError(res, error);
     }
   }
 
@@ -100,12 +93,16 @@ export class UserController {
         return res.status(NOT_FOUND).json({ message: "User not found" });
       }
       userModel.remove(userToRemove);
-      res.status(NO_CONTENT).json({ message: "User deleted successfully" });
+      res.status(OK).json({ message: "User deleted successfully" });
     } catch (error) {
-      console.log(error);
-      res
-        .status(INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
+      UserController.handleServerError(res, error);
     }
+  }
+
+  static handleServerError(res: Response, error: Error) {
+    console.error(`Error in UserController: ${error.message}`);
+    return res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
   }
 }
--- app/utils/general.ts
+++ app/utils/general.ts
@@ -5,3 +5,7 @@ dotenv.config();
 export function getEnv(env: string) {
   return process.env[env] || "";
 }
+
+export function isAString(inputString: string) {
+  return typeof inputString === "string" && inputString.trim() !== "";
+}
