b673a26874a3d5995ca026306fe299cc803abfcb
ansman58 <javanslem@gmail.com>
2023-09-13 19:12:15 +0100

    fix ormconfig file bug

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,8 @@
 import { getEnv } from "./app/utils/general";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { Users } from "./app/models/Person";
+import { User1694465023342 } from "./app/migrations/1694465023342-person";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -8,9 +10,9 @@ export const AppDataSource = new DataSource({
   username: getEnv("DB_USER"),
   password: getEnv("DB_PASSWORD"),
   database: getEnv("DB_NAME"),
-  entities: ["app/models/*.ts"],
+  entities: [Users],
   logging: false,
-  migrations: ["app/migrations/*.ts"],
+  migrations: [User1694465023342],
   migrationsTableName: "typeorm-schema",
   migrationsRun: true,
   synchronize: false,
